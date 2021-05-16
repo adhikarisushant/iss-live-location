4b10002517fb0f0dfaaed9a4e208426adf455843
Sushant <sushant.adhikari.619@gmail.com>
2021-05-16 14:20:51 +0545

    refreshing data with setInterval()

--- script.js
+++ script.js
@@ -18,16 +18,24 @@ const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
+    let firstTime = true;
+
 async function getISS() {
     const response = await  fetch(api_url);
     const data = await response.json();
     const { latitude, longitude } = data;
 
-    // L.marker([latitude, longitude]).addTo(mymap);
     marker.setLatLng([latitude, longitude]);
+    if (firstTime) {
+        mymap.setView([latitude, longitude], 2);
+        firstTime = false;
+    }
 
-    document.getElementById('lat').textContent = latitude;
-    document.getElementById('lon').textContent = longitude;
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);
 }
 
 getISS();
+
+
+setInterval(getISS, 1000);
